d2d1a7dec27242a619804b80bb00aa2d0dc1b7be
Nicholas Escalona <nicholas.escalona@gmail.com>
2015-08-27 00:31:56 -0400

    added region checking to check-titles

--- esc/check_titles.py
+++ esc/check_titles.py
@@ -3,6 +3,7 @@
 import collections
 import csv
 import pathlib
+import pprint
 import re
 import sys
 import shutil
@@ -55,6 +56,36 @@ def check_titles(path, titles):
         print(path)
         raise
 
+def check_regions(titles, titles_de_jure):
+    bad_titles = []
+
+    def check_region(n):
+        if ck2parser.is_codename(n.val) and n.val not in titles_de_jure:
+            bad_titles.append(n.val)
+
+    def recurse(tree):
+        if tree.has_pairs:
+            for p in tree:
+                n, v = p
+                v_is_obj = isinstance(v, ck2parser.Obj)
+                check_title(n, path, titles, v_is_obj, p)
+                check_region(n)
+                if v_is_obj:
+                    recurse(v)
+                else:
+                    check_title(v, path, titles, line=p)
+                    check_region(v)
+        else:
+            for v in tree:
+                check_title(v, path, titles, line=v)
+                check_region(v)
+
+    path = modpath / 'map/geographical_region.txt'
+    tree = ck2parser.parse_file(path)
+    recurse(tree)
+    return bad_titles
+
+
 def check_province_history(titles):
     tree = ck2parser.parse_file(modpath / 'map/default.map')
     defs = next(v.val for n, v in tree if n.val == 'definitions')
@@ -74,25 +105,37 @@ def check_province_history(titles):
 
 def process_landed_titles():
     titles = set()
+    titles_de_jure = set()
 
     def recurse(tree):
+        parent_is_titular = True
         for n, v in tree:
             if ck2parser.is_codename(n.val):
                 if n.val in titles:
                     print('Duplicate title {}'.format(n.val))
                 titles.add(n.val)
-                recurse(v)
+                if n.val.startswith('b_'):
+                    titles_de_jure.add(n.val)
+                    parent_is_titular = False
+                else:
+                    is_titular = recurse(v)
+                    if not is_titular:
+                        titles_de_jure.add(n.val)
+                        parent_is_titular = False
+        return parent_is_titular
 
     for path in ck2parser.files('common/landed_titles/*.txt', modpath):
         recurse(ck2parser.parse_file(path))
-    return titles
+    # print('{} titles, {} de jure'.format(len(titles), len(titles_de_jure)))
+    return titles, titles_de_jure
 
 def main():
     global modpath
     if len(sys.argv) > 1:
         modpath = pathlib.Path(sys.argv[1])
-    titles = process_landed_titles()
+    titles, titles_de_jure = process_landed_titles()
     check_province_history(titles)
+    bad_region_titles = check_regions(titles, titles_de_jure)
     for path in ck2parser.files('history/titles/*.txt', modpath):
         tree = ck2parser.parse_file(path)
         if tree.contents:
@@ -116,12 +159,15 @@ def main():
         'common/cb_types/*.txt',
         'common/scripted_triggers/*.txt',
         'common/scripted_effects/*.txt',
-        'common/achievements.txt',
+        'common/achievements.txt'
         ]
     for glob in globs:
         for path in ck2parser.files(glob, modpath):
             check_titles(path, titles)
     with (rootpath / 'out.txt').open('w', encoding='cp1252') as fp:
+        if bad_region_titles:
+            print('Invalid or titular titles in regions:\n\t', end='', file=fp)
+            print(*bad_region_titles, sep=' ', file=fp)
         for lhs in [True, False]:
             if results[lhs]:
                 if lhs:
