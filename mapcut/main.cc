bfda14b51e174c15d09e836392e2901f6a5e956c
Matthew D. Hall <zijistark@gmail.com>
2015-09-24 17:35:10 -0700

    mapcut: some sanity for main()

--- mapcut/main.cc
+++ mapcut/main.cc
@@ -23,7 +23,9 @@ void print_stmt(int indent, const pdx::stmt&);
 void print_obj(int indent, const pdx::obj&);
 
 typedef std::vector<std::string> strvec_t;
+typedef std::unordered_map<std::string, uint> str2id_map_t;
 
+void fill_county_to_id_map(const default_map&, const definitions_table&, str2id_map_t& out);
 const pdx::block* find_title(const char* title, const pdx::block* p_root);
 void find_titles_under(const pdx::block*, strvec_t& out);
 
@@ -42,78 +44,10 @@ int main(int argc, char** argv) {
 
     try {
         default_map dm(ROOT_DIR.string());
-
         definitions_table def_tbl(dm);
 
-        std::unordered_map<std::string, uint> county_to_id_map;
-
-        path prov_hist_root = ROOT_DIR / "history/provinces";
-        path prov_hist_vroot = VROOT_DIR / "history/provinces";
-
-        for (uint i = 0; i < def_tbl.row_vec.size(); ++i) {
-            const definitions_table::row& r = def_tbl.row_vec[i];
-            const uint id = i+1;
-
-            if (dm.id_is_seazone(id)) // sea | major river
-                continue;
-
-            if (r.name.empty()) // wasteland | external
-                continue;
-
-            char filename[128];
-            sprintf(filename, "%u - %s.txt", id, r.name.c_str());
-
-            path prov_hist_file = prov_hist_root / filename;
-
-            if (!exists(prov_hist_file)) {
-
-                path prov_hist_vfile = prov_hist_vroot / filename;
-
-                if (!exists(prov_hist_vfile))
-                    throw va_error("could not find province history file: %s",
-                                   filename);
-                else {
-                    if (false) {
-                        /* SWMH doesn't want to rely upon inherited vanilla province
-                           history, so fix this situation right now. note that is
-                           definitely not general-purpose behavior. */
-                        copy_file(prov_hist_vfile, prov_hist_file);
-                    }
-                    else {
-                        prov_hist_file = prov_hist_vfile;
-                    }
-                }
-            }
-
-            assert( exists(prov_hist_file) );
-
-            const char* county = nullptr;
-
-            pdx::plexer lex(prov_hist_file.c_str());
-            pdx::block doc(lex, true);
-
-            for (auto&& s : doc.stmt_list) {
-                if (s.key_eq("title")) {
-                    assert( s.val.type == pdx::obj::TITLE );
-                    county = s.val.data.s;
-                    assert( pdx::title_tier(county) == pdx::TIER_COUNT );
-                }
-            }
-
-            if (county == nullptr) {
-                /* history file contained no title assignment.  it was probably
-                   blank (for a wasteland or something). we may want to warn the
-                   user about this, although the behavior of CK2's error.log
-                   would suggest that empty history files ought be used for
-                   wasteland (which I believe is incorrect). */
-                continue;
-            }
-
-            if (!county_to_id_map.insert( {county, id} ).second) {
-                throw va_error("county '%s' maps to both province %u and %u (at the least)!",
-                               county, county_to_id_map[county], id);
-            }
-        }
+        str2id_map_t county_to_id_map;
+        fill_county_to_id_map(dm, def_tbl, county_to_id_map);
 
         const path titles_path = ROOT_DIR / TITLES_PATH;
         pdx::plexer lex(titles_path.c_str());
@@ -184,6 +118,82 @@ void find_titles_under(const pdx::block* p_root, strvec_t& found_titles) {
             find_titles_under(s.val.as_block(), found_titles);
     }
 }
+
+
+
+void fill_county_to_id_map(const default_map& dm,
+                           const definitions_table& def_tbl,
+                           str2id_map_t& county_to_id_map) {
+
+    path prov_hist_root = ROOT_DIR / "history/provinces";
+    path prov_hist_vroot = VROOT_DIR / "history/provinces";
+
+    for (uint i = 0; i < def_tbl.row_vec.size(); ++i) {
+        const definitions_table::row& r = def_tbl.row_vec[i];
+        const uint id = i+1;
+
+        if (dm.id_is_seazone(id)) // sea | major river
+            continue;
+
+        if (r.name.empty()) // wasteland | external
+            continue;
+
+        char filename[128];
+        sprintf(filename, "%u - %s.txt", id, r.name.c_str());
+
+        path prov_hist_file = prov_hist_root / filename;
+
+        if (!exists(prov_hist_file)) {
+
+            path prov_hist_vfile = prov_hist_vroot / filename;
+
+            if (!exists(prov_hist_vfile))
+                throw va_error("could not find province history file: %s",
+                               filename);
+            else {
+                if (false) {
+                    /* SWMH doesn't want to rely upon inherited vanilla province
+                       history, so fix this situation right now. note that is
+                       definitely not general-purpose behavior. */
+                    copy_file(prov_hist_vfile, prov_hist_file);
+                }
+                else {
+                    prov_hist_file = prov_hist_vfile;
+                }
+            }
+        }
+
+        assert( exists(prov_hist_file) );
+
+        const char* county = nullptr;
+
+        pdx::plexer lex(prov_hist_file.c_str());
+        pdx::block doc(lex, true);
+
+        for (auto&& s : doc.stmt_list) {
+            if (s.key_eq("title")) {
+                assert( s.val.type == pdx::obj::TITLE );
+                county = s.val.data.s;
+                assert( pdx::title_tier(county) == pdx::TIER_COUNT );
+            }
+        }
+
+        if (county == nullptr) {
+            /* history file contained no title assignment.  it was probably
+               blank (for a wasteland or something). we may want to warn the
+               user about this, although the behavior of CK2's error.log
+               would suggest that empty history files ought be used for
+               wasteland (which I believe is incorrect). */
+            continue;
+        }
+
+        if (!county_to_id_map.insert( {county, id} ).second) {
+            throw va_error("county '%s' maps to both province %u and %u (at the least)!",
+                           county, county_to_id_map[county], id);
+        }
+    }
+}
+
 
 void print_block(int indent, const pdx::block* p_b) {
     for (auto&& s : p_b->stmt_list)
