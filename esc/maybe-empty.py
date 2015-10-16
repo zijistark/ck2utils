6b8a69515c5d2651c2cc27db9800df3b76c15e51
Nicholas Escalona <nicholas.escalona@gmail.com>
2015-10-15 23:23:37 -0400

    maybe-empty: look for absence of cities and castles at any time: not presence of tribals at any time

--- esc/maybe-empty.py
+++ esc/maybe-empty.py
@@ -24,21 +24,35 @@ def process_provinces(where):
         except (IndexError, ValueError):
             continue
     province_id = {}
-    tribals = set()
+    no_castles_or_cities = set()
     for path in ck2parser.files('history/provinces/*', where):
         number, name = path.stem.split(' - ')
         number = int(number)
         if id_name[number] == name:
             tree = ck2parser.parse_file(path)
+            castles_and_cities = set()
+            changes_by_date = {}
             for n, v in tree:
-                if isinstance(v, ck2parser.Obj):
-                    if any(v2.val == 'tribal' for _, v2 in v):
-                        tribals.add(number)
-                elif n.val == 'title':
+                if n.val == 'title':
                     province_id[v.val] = number
-                elif v.val == 'tribal':
-                    tribals.add(number)
-    return province_id, tribals
+                elif (isinstance(n, ck2parser.Date) and
+                      isinstance(v, ck2parser.Obj)):
+                    changes_by_date[n.val] = v.contents
+                elif v.val == 'castle' or v.val == 'city':
+                    castles_and_cities.add(n.val)
+            if not castles_and_cities:
+                no_castles_or_cities.add(number)
+                continue
+            for _, changes in sorted(changes_by_date.items()):
+                for n, v in changes:
+                    if n.val == 'remove_settlement':
+                        castles_and_cities.discard(v.val)
+                    elif v.val == 'castle' or v.val == 'city':
+                        castles_and_cities.add(n.val)
+                if not castles_and_cities:
+                    no_castles_or_cities.add(number)
+                    break
+    return province_id, no_castles_or_cities
 
 def process_titles(where):
     nomads = set()
@@ -60,14 +74,14 @@ def process_titles(where):
 
 def main():
     modpath = get_modpath()
-    province_id, tribals = process_provinces(modpath)
+    province_id, no_castles_or_cities = process_provinces(modpath)
     nomads, vassals = process_titles(modpath)
     maybe_empty = set()
 
     def check_nomad(title):
         if title.startswith('c'):
             number = province_id[title]
-            if number in tribals:
+            if number in no_castles_or_cities:
                 maybe_empty.add(number)
         else:
             for vassal in vassals[title]:
