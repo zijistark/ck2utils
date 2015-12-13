441e3bc987f7a73ab9b25434b9ad7f4fcb7a47c1
Matthew D. Hall <zijistark@gmail.com>
2015-12-13 10:34:41 -0800

    evtlog_filter: supported nested indent/multi-line logs

--- logs/evtlog_filter.pl
+++ logs/evtlog_filter.pl
@@ -1,24 +1,24 @@
 #!/usr/bin/perl
 
-my $ml = 0;
+my $indent = 0;
 
 while (<>) {
 	next unless /^.+?EVENT .(\d+)\.(\d+)\.(\d+).:(.+)$/;
 	my ($y, $m, $d, $msg) = ($1, $2, $3, $4);
 	
 	if ($msg =~ '>>>') {
-		$ml = 0;
+		--$indent;
 		next;
 	}
-	elsif ($msg =~ '<<<') {
-		$ml = 1;
+	if ($msg =~ '<<<') {
+		++$indent;
 		next;
 	}
 	
 	$msg = "\a$msg" if ($msg =~ 'ASSERT' || $msg =~ 'SERIOUS' || $msg =~ 'ERROR');
 	
-	if ($ml) {
-		print "> $msg\n";
+	if ($indent) {
+		print(('>' x $indent), " $msg\n");
 	}
 	else {
 		printf("%d.%02d.%02d> %s\n", $y, $m, $d, $msg);
