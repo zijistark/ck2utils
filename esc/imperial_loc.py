1c75c5944b250ddbb7f3dd0f8a8edf435593530c
Nicholas Escalona <nicholas.escalona@gmail.com>
2016-07-02 09:39:26 -0400

    imperial_loc: fix oversights with vice_royalty locs

--- esc/imperial_loc.py
+++ esc/imperial_loc.py
@@ -6,9 +6,10 @@ from ck2parser import (rootpath, get_cultures, get_religions, files, csv_rows,
                        SimpleParser)
 from print_time import print_time
 
-# EMF+Vanilla or EMF+SWMH
-MODDIRS, OUTPUT = ['EMF/EMF', 'EMF/EMF+Vanilla'], 'EMF/EMF'
-#MODDIRS, OUTPUT = ['EMF/EMF', 'SWMH-BETA/SWMH', 'EMF/EMF+SWMH'], 'EMF/EMF+SWMH'
+# EMF+Vanilla, EMF+SWMH
+MODDIRS = (['EMF/EMF', 'EMF/EMF+Vanilla'],
+           ['EMF/EMF', 'SWMH-BETA/SWMH', 'EMF/EMF+SWMH'])
+OUTPUT = 'EMF/EMF', 'EMF/EMF+SWMH'
 
 OUT_NAME = 'zz~imperial_titles_unmodified.csv'
 
@@ -51,10 +52,13 @@ def write_output(out_path, parser, loc_keys, noble_regex):
             if key in seen or not re.fullmatch(noble_regex, key):
                 continue
             if key.startswith('vice_royalty'):
-                if 'empire' not in key and 'emperor' not in key:
+                if not any(x in key for x in
+                           ['empire', 'emperor', 'count', 'baron']):
                     imperial_key = 'imperial' + key[len('vice_royalty'):]
-                    viceroy_rows.append(([imperial_key] + row[1:14] +
-                                        [''] * (14 - len(row)) + ['x']))
+                    viceroy_row = ([imperial_key] + row[1:14] +
+                                   [''] * (14 - len(row)) + ['x'])
+                    if not any(r[0] == viceroy_row[0] for r in viceroy_rows):
+                        viceroy_rows.append(viceroy_row)
                 continue
             seen.add(key)
             imperial_key = 'imperial_' + key
@@ -63,28 +67,34 @@ def write_output(out_path, parser, loc_keys, noble_regex):
                            [''] * (14 - len(row)) + ['x'])
                 out_rows.append(out_row)
     for out_row in out_rows:
-        try:
-            viceroy_row = next(row for row in viceroy_rows
-                               if row[0] == out_row[0])
-        except StopIteration:
-            continue
-        out_row[:] = viceroy_row
+        for i, row in enumerate(viceroy_rows):
+            if row[0] == out_row[0]:
+                viceroy_rows.pop(i)
+                out_row[:] = row
+                break
+    if viceroy_rows:
+        out_rows.append(['#[extra vice_royalty keys]'] + [''] * 13 + ['x'])
+        out_rows.extend(viceroy_rows)
     with out_path.open('w', encoding='cp1252', newline='') as csvfile:
         csv.writer(csvfile, dialect='ckii').writerows(out_rows)
 
-@print_time
-def main():
-    parser = SimpleParser()
-    parser.moddirs = [rootpath / d for d in MODDIRS]
+def run_with_mod_set(parser, moddirs, output):
+    parser.moddirs = moddirs
     loc_keys = get_localisation_keys(parser.moddirs)
     cultures, cult_groups = get_cultures(parser)
     religions, rel_groups = get_religions(parser)
     other_titles = get_other_titles(parser)
     noble_regex = make_noble_title_regex(
         cultures + cult_groups, religions + rel_groups, other_titles)
-    out_path = (rootpath / OUTPUT /
-                'localisation/zz~imperial_titles_unmodified.csv')
+    out_path = output / 'localisation/zz~imperial_titles_unmodified.csv'
     write_output(out_path, parser, loc_keys, noble_regex)
+
+@print_time
+def main():
+    parser = SimpleParser()
+    for moddirs, output in zip(MODDIRS, OUTPUT):
+        run_with_mod_set(parser,
+                         [rootpath / d for d in moddirs], rootpath / output)
 
 if __name__ == '__main__':
     main()
