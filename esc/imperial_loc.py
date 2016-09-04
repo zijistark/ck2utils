3143cbb997b2b132c0ed0d4c6235cee313bf639d
Nicholas Escalona <nicholas.escalona@gmail.com>
2016-09-03 21:04:53 -0400

    imperial_loc: skip everything but the vice_royalty stuff

--- esc/imperial_loc.py
+++ esc/imperial_loc.py
@@ -11,7 +11,7 @@ MODDIRS = (['EMF/EMF', 'EMF/EMF+Vanilla'],
            ['EMF/EMF', 'SWMH-BETA/SWMH', 'EMF/EMF+SWMH'])
 OUTPUT = 'EMF/EMF', 'EMF/EMF+SWMH'
 
-OUT_NAME = 'zz~imperial_titles_unmodified.csv'
+OUT_NAME = '1_imperial_titles_unmodified.csv'
 
 def get_localisation_keys(moddirs):
     loc_keys = set()
@@ -46,7 +46,7 @@ def write_output(out_path, parser, loc_keys, noble_regex):
     for path in files('localisation/*', parser.moddirs):
         if path.name == OUT_NAME:
             continue
-        out_rows.append(['#' + path.name] + [''] * 13 + ['x'])
+        # out_rows.append(['#' + path.name] + [''] * 13 + ['x'])
         for row in csv_rows(path):
             key = row[0]
             if key in seen or not re.fullmatch(noble_regex, key):
@@ -61,19 +61,19 @@ def write_output(out_path, parser, loc_keys, noble_regex):
                         viceroy_rows.append(viceroy_row)
                 continue
             seen.add(key)
-            imperial_key = 'imperial_' + key
-            if imperial_key not in loc_keys:
-                out_row = ([imperial_key] + row[1:14] +
-                           [''] * (14 - len(row)) + ['x'])
-                out_rows.append(out_row)
-    for out_row in out_rows:
-        for i, row in enumerate(viceroy_rows):
-            if row[0] == out_row[0]:
-                viceroy_rows.pop(i)
-                out_row[:] = row
-                break
+            # imperial_key = 'imperial_' + key
+            # if imperial_key not in loc_keys:
+            #     out_row = ([imperial_key] + row[1:14] +
+            #                [''] * (14 - len(row)) + ['x'])
+            #     out_rows.append(out_row)
+    # for out_row in out_rows:
+    #     for i, row in enumerate(viceroy_rows):
+    #         if row[0] == out_row[0]:
+    #             viceroy_rows.pop(i)
+    #             out_row[:] = row
+    #             break
     if viceroy_rows:
-        out_rows.append(['#[extra vice_royalty keys]'] + [''] * 13 + ['x'])
+        # out_rows.append(['#[extra vice_royalty keys]'] + [''] * 13 + ['x'])
         out_rows.extend(viceroy_rows)
     with out_path.open('w', encoding='cp1252', newline='') as csvfile:
         csv.writer(csvfile, dialect='ckii').writerows(out_rows)
@@ -86,7 +86,7 @@ def run_with_mod_set(parser, moddirs, output):
     other_titles = get_other_titles(parser)
     noble_regex = make_noble_title_regex(
         cultures + cult_groups, religions + rel_groups, other_titles)
-    out_path = output / 'localisation/zz~imperial_titles_unmodified.csv'
+    out_path = output / 'localisation' / OUT_NAME
     write_output(out_path, parser, loc_keys, noble_regex)
 
 @print_time
