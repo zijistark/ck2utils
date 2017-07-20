b113d2b0fa7ee8e5c59e231c40a8f59169086b8b
Nicholas Escalona <nicholas.escalona@gmail.com>
2017-07-19 21:17:16 -0400

    keep alpha modules folder separate

--- esc/hipinstall.sh
+++ esc/hipinstall.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd /home/Nicholas/mod
+[ -e modules ] && mv modules{,-backup}
+mv modules{-alpha,}
+trap "mv modules{,-alpha} ; [ -e modules-backup ] && mv modules{-backup,}" EXIT
 date -I > modules/version.txt
 /home/Nicholas/CK2/HIP-tools/installer/main.py --in-place "$@"
