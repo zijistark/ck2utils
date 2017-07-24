7bd47048a3e769b009e24c8275a041c21f5fe5e2
Nicholas Escalona <nicholas.escalona@gmail.com>
2017-07-24 02:07:50 -0400

    found source of culture group colors in game files

--- esc/eu4culture_map.py
+++ esc/eu4culture_map.py
@@ -13,42 +13,6 @@ from ck2parser import rootpath, csv_rows, SimpleParser, Obj
 from localpaths import eu4dir
 from print_time import print_time
 
-CULTURE_GROUP_COLOR = {
-    'germanic': '#DC8A39',          'scandinavian': '#3F7DD6',
-    'british': '#36A79C',           'gaelic': '#DB7C8B',
-    'latin': '#39A065',             'iberian': '#C28C56',
-    'french': '#C7AF0C',            'finno_ugric': '#459DD0',
-    'south_slavic': '#75A5BC',      'west_slavic': '#CEB561',
-    'carpathian': '#C3959B',        'east_slavic': '#5E88BF',
-    'baltic': '#E7B50C',            'byzantine': '#926C92',
-    'caucasian': '#4582CB',         'turko_semitic': '#3B9E7D',
-    'maghrebi': '#0490B2',          'iranian': '#BF7185',
-    'altaic': '#939D4C',            'central_american': '#1A35B1',
-    'maya': '#7D5C6E',              'otomanguean': '#79A26D',
-    'andean_group': '#B68664',      'je_tupi': '#C9614A',
-    'je': '#745C27',                'maranon': '#216589',
-    'chibchan': '#81B17D',          'mataco': '#766397',
-    'araucanian': '#17728F',        'carribean': '#6EAD81',
-    'eskaleut': '#941E46',          'central_algonquian': '#645E7D',
-    'plains_algonquian': '#287942', 'eastern_algonquian': '#2959AE',
-    'iroquoian': '#F0A782',         'siouan': '#C14136',
-    'caddoan': '#76C199',           'muskogean': '#5DB44C',
-    'sonoran': '#78948C',           'na_dene': '#A18B28',
-    'penutian': '#246DC2',          'east_asian': '#8CCF74',
-    'korean_g': '#82194A',          'japanese_g': '#B2A38F',
-    'mon_khmer': '#9A8123',         'malay': '#75A143',
-    'thai': '#844666',              'burman': '#724667',
-    'pacific': '#5E7537',           'eastern_aryan': '#3E7ABD',
-    'hindusthani': '#C11A0E',       'western_aryan': '#DC8A39',
-    'dravidian': '#889D17',         'central_indic': '#30A433',
-    'mande': '#B68664',             'sahelian': '#276C8C',
-    'west_african': '#709669',      'southern_african': '#BF7185',
-    'kongo_group': '#6B5EA9',       'great_lakes_group': '#79A26D',
-    'african': '#75A5BC',           'cushitic': '#CA7055',
-    'sudanese': '#68898C',          'evenks': '#DC969E',
-    'kamchatkan_g': '#A64839',      'tartar': '#C7AF0C'
-}
-
 @print_time
 def main():
     parser = SimpleParser()
@@ -75,14 +39,20 @@ def main():
             rgb = tuple(np.uint8(row[1:4]))
             rgb_number_map[rgb] = np.uint16(number)
 
-    group_cultures = defaultdict(list)
+    grouped_cultures = []
     for _, tree in parser.parse_files('common/cultures/*'):
         for n, v in tree:
-            if n.val in CULTURE_GROUP_COLOR:
-                for n2, v2 in v:
-                    if (isinstance(v2, Obj) and
-                        not re.match(r'((fe)?male|dynasty)_names', n2.val)):
-                        group_cultures[n.val].append(n2.val)
+            cultures = []
+            for n2, v2 in v:
+                if (isinstance(v2, Obj) and
+                    not re.match(r'((fe)?male|dynasty)_names', n2.val)):
+                    cultures.append(n2.val)
+            grouped_cultures.append(cultures)
+
+    region_colors = []
+    for _, tree in parser.parse_files('common/region_colors/*'):
+        for n, v in tree:
+            region_colors.append(spectra.rgb(*(n2.val / 255 for n2 in v)))
 
     culture_color = {'noculture': colors['land']}
     spherical_code = {
@@ -92,8 +62,8 @@ def main():
             (-1 / 2, -math.sqrt(3) / 2, 0)]
     }
     out_of_gamut = 0
-    for group, cultures in group_cultures.items():
-        group_color = spectra.html(CULTURE_GROUP_COLOR[group]).to('lab').values
+    for i, cultures in enumerate(grouped_cultures):
+        group_color = region_colors[i + 1].to('lab').values
         num_cultures = len(cultures)
         try:
             code = spherical_code[num_cultures]
@@ -112,7 +82,7 @@ def main():
                 out_of_gamut += 1
             upscaled = [round(x * 255) for x in color.clamped_rgb]
             culture_color[culture] = np.uint8(upscaled)
-    culture_count = sum(len(x) for x in group_cultures.values())
+    culture_count = sum(len(x) for x in grouped_cultures)
     print('Out of gamut: {:.2%}'.format(out_of_gamut / culture_count),
           file=sys.stderr)
 
