885fd2125ff889cae1ea8a6a5394ba743d830d67
Nicholas Escalona <nicholas.escalona@gmail.com>
2017-11-12 03:52:35 -0500

    here too

--- esc/hipinstall.sh
+++ esc/hipinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /home/Nicholas/mod
+cd ${HOME}/mod
 [ -e modules ] && mv modules{,-backup}
 mv modules{-alpha,}
 trap "mv modules{,-alpha} ; [ -e modules-backup ] && mv modules{-backup,}" EXIT
 date -I > modules/version.txt
-/home/Nicholas/CK2/HIP-tools/installer/main.py --in-place "$@"
+${REPO_ROOT}/HIP-tools/installer/main.py --in-place "$@"
