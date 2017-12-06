3c270a7bb7eb5268b2380e3791f1347c1160328e
Nicholas Escalona <nicholas.escalona@gmail.com>
2017-12-06 16:58:47 -0500

    document province_setup usage

--- esc/province_setup.py
+++ esc/province_setup.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
 
-# REMEMBER TO:
-# copy EMF's emf_core_custom_loc.txt and 1_emf_debug.csv
-# delete 00_province_setup.txt
+# USAGE:
+# log_province_setup.py
+# mkdir -p $REPO_ROOT/SWMH-BETA/SWMH/localisation/customizable_localisation
+# cp $REPO_ROOT/{EMF/EMF,SWMH-BETA/SWMH}/localisation/customizable_localisation/emf_debug_custom_loc.txt
+# cp $REPO_ROOT/{EMF/EMF,SWMH-BETA/SWMH}/localisation/1_emf_debug.csv
+# rm $REPO_ROOT/SWMH-BETA/SWMH/common/province_setup/*
+# hipinstall.sh --swmh
+# ~/common/Crusader\ Kings\ II/CK2game.exe -debug -debugscripts
+# # console "run province_setup.txt", quit
+# cp ~/win/Documents/Paradox\ Interactive/Crusader\ Kings\ II/logs/game.log $REPO_ROOT/province_setup_data.txt
+# province_setup.py # run this file
+# git -C $REPO_ROOT/SWMH-BETA clean -df
 
 
 import re
@@ -20,9 +29,11 @@ def main():
     parser = ck2parser.FullParser(ck2parser.rootpath / 'SWMH-BETA/SWMH')
     if NEW_DATA_FROM_FILE:
         output_tree = parser.parse('')
-        with NEW_DATA_FROM_FILE.open(encoding='utf8') as f:
+        with NEW_DATA_FROM_FILE.open(encoding='cp1252') as f:
             for line in f:
-                match = re.match(r'<(.*?)> (.*)', line)
+                match = re.search(r'<(.*?)> (.*)', line)
+                if match is None:
+                    continue
                 prov_type, pairs = match.groups()
                 data = dict(x.split('=') for x in pairs.split(', '))
                 to_parse = '{} = {{\n'.format(data['id'])
@@ -33,7 +44,11 @@ def main():
                     data['max_settlements'] if prov_type == 'LAND' else 7)
                 to_parse += 'terrain = {}\n'.format(data['terrain'])
                 to_parse += '}\n'
-                parsed = parser.parse(to_parse)
+                try:
+                    parsed = parser.parse(to_parse)
+                except:
+                    print(repr(to_parse))
+                    raise
                 output_tree.contents.extend(parsed)
         parsed = parser.parse('# -*- ck2.province_setup -*-')
         output_tree.pre_comments[:0] = parsed.post_comments
