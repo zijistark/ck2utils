5d54373fd3d2296f1497c5f23dac49fbe8e85f00
Matthew D. Hall <zijistark@gmail.com>
2018-07-23 18:06:21 -0700

    derp, forgot a few new files

---
+++ libck2/src/ck2/ProvinceMap.cc
@@ -0,0 +1,129 @@
+
+#include "BMPHeader.h"
+#include "FileLocation.h"
+#include "ProvinceMap.h"
+#include <cstdio>
+#include <cerrno>
+#include <cstdlib>
+#include <string>
+#include <unordered_map>
+
+
+_CK2_NAMESPACE_BEGIN;
+
+
+ProvinceMap::ProvinceMap(const VFS& vfs, const DefaultMap& dm, const DefinitionsTable& def_tbl)
+: _up_map(nullptr),
+  _n_width(0),
+  _n_height(0)
+{
+    /* map provinces.bmp color to province ID */
+    std::unordered_map<rgb, uint16_t> color2id_map;
+
+    for (const auto& row : def_tbl)
+        color2id_map.emplace(row.color, row.id);
+
+    const auto path = vfs["map" / dm.province_map_path()];
+    const std::string spath = path.generic_string();
+
+    // unique_file_ptr will automatically destroy/close its FILE* if we throw an exception (or return)
+    typedef std::unique_ptr<std::FILE, int (*)(std::FILE *)> unique_file_ptr;
+    unique_file_ptr ufp( std::fopen(spath.c_str(), "rb"), std::fclose );
+
+    if (ufp.get() == nullptr)
+        throw Error("Failed to open file: {}: {}", strerror(errno), spath);
+
+    BMPHeader bf_hdr;
+
+    if (fread(&bf_hdr, sizeof(bf_hdr), 1, ufp.get()) < 1)
+    {
+        if (errno)
+            throw FLError(path, "Failed to read bitmap file header: {}", strerror(errno));
+        else
+            throw FLError(path, "Unexpected EOF while reading bitmap file header (corrupted?)");
+    }
+
+    if (bf_hdr.magic != BMPHeader::MAGIC)
+        throw FLError(path, "Unsupported bitmap file type (magic={0:04x} but want magic={0:04x})",
+                      bf_hdr.magic, BMPHeader::MAGIC);
+
+    // TODO: all these not to be handled by compile-time assertions (every damn one from here one!)
+    assert( bf_hdr.n_header_size >= 40 );
+    assert( bf_hdr.n_width > 0 );
+    assert( bf_hdr.n_height > 0 ); // TODO (though not sure if game supports top-to-bottom scan order)
+    assert( bf_hdr.n_planes == 1 );
+    assert( bf_hdr.n_bpp == 24 );
+    assert( bf_hdr.compression_type == 0 );
+    assert( bf_hdr.n_colors == 0 );
+    assert( bf_hdr.n_important_colors == 0 );
+
+    _n_width = bf_hdr.n_width;
+    _n_height = bf_hdr.n_height;
+
+    /* calculate row size with 32-bit alignment padding */
+    uint n_row_sz = 4 * ((bf_hdr.n_bpp * _n_width + 31) / 32);
+
+    if (bf_hdr.n_bitmap_size)
+        assert( bf_hdr.n_bitmap_size == n_row_sz * _n_height );
+
+    /* allocate ID map */
+    _up_map = std::make_unique<uint16_t[]>(_n_width * _n_height);
+
+    /* seek past any other bytes and directly to offset of pixel array (if needed). */
+    if (fseek(ufp.get(), bf_hdr.n_bitmap_offset, SEEK_SET) != 0)
+        throw FLError(path, "Failed to seek to raw bitmap data (offset={:X}): %s",
+                      bf_hdr.n_bitmap_offset,
+                      strerror(errno));
+
+    /* read bitmap image data (pixel array), row by row, in bottom-to-top raster scan order */
+
+    uint8_t* p_row = new uint8_t[n_row_sz];
+
+    for (uint row = 0; row < _n_height; ++row)
+    {
+        if (fread(p_row, n_row_sz, 1, ufp.get()) < 1)
+        {
+            if (errno)
+                throw FLError(path, "Failed to read bitmap file data: {}", strerror(errno));
+            else
+                throw FLError(path, "Unexpected EOF while reading bitmap file data");
+        }
+
+        const uint y = _n_height - 1 - row;
+
+        /* cache previous pixel's value & province ID */
+        uint8_t  prev_b = 0;
+        uint8_t  prev_g = 0;
+        uint8_t  prev_r = 0;
+        uint16_t prev_id = 0;
+
+        for (uint x = 0; x < _n_width; ++x)
+        {
+            const uint8_t* p = &p_row[3*x];
+            uint16_t id;
+
+            if (p[0] == 0xFF && p[1] == 0xFF && p[2] == 0xFF)
+                id = PM_OCEAN;
+            else if (p[0] == 0x00 && p[1] == 0x00 && p[2] == 0x00)
+                id = PM_IMPASSABLE;
+            else if (x > 0 && p[0] == prev_b && p[1] == prev_g && p[2] == prev_r)
+                id = prev_id; // save time at the bottleneck of the hash table lookup due to color locality
+            else
+            {
+                if (auto it = color2id_map.find({ p[2], p[1], p[0] }); it != color2id_map.end())
+                    id = it->second;
+                else
+                    throw FLError(path, "Unexpected color RGB(%hhu, %hhu, %hhu) in provinces bitmap at (%u, %u)",
+                                  p[2], p[1], p[0], x, y);
+            }
+
+            prev_b = p[0];
+            prev_g = p[1];
+            prev_r = p[2];
+            prev_id = _up_map[y*_n_width + x] = id;
+        }
+    }
+}
+
+
+_CK2_NAMESPACE_END;
---
+++ libck2/src/ck2/ProvinceMap.h
@@ -0,0 +1,47 @@
+#ifndef __LIBCK2_PROVINCES_MAP_H__
+#define __LIBCK2_PROVINCES_MAP_H__
+
+#include "common.h"
+#include "VFS.h"
+#include "DefaultMap.h"
+#include "DefinitionsTable.h"
+#include "filesystem.h"
+#include <memory>
+#include <limits>
+
+
+_CK2_NAMESPACE_BEGIN;
+
+
+// A ProvinceMap class opens & processes the raw bitmap from 'provinces.bmp' & allocates it into a buffer(s) of
+// 16-bit province IDs. It then provides read-only access to that mapping of IDs to raster (x, y) grid coordinates.
+// Some province IDs at the very top of the ID range are reserved for useful classifications.
+
+// API NOTE: Never assume the actual memory representation of the grid is indeed one big, rectangular grid. Ergo,
+// always access grid contents via the operator(x, y) overload. [didn't feel like dealing with proxy overloads for
+// operator[].]
+class ProvinceMap
+{
+public:
+    ProvinceMap() = delete;
+    ProvinceMap(const VFS&, const DefaultMap&, const DefinitionsTable&);
+
+    static constexpr uint16_t PM_OCEAN      = std::numeric_limits<uint16_t>::max() - 0;
+    static constexpr uint16_t PM_IMPASSABLE = std::numeric_limits<uint16_t>::max() - 1;
+    static constexpr uint16_t REAL_ID_MAX   = std::numeric_limits<uint16_t>::max() - 2;
+
+    uint width() const noexcept  { return _n_width; }
+    uint height() const noexcept { return _n_height; }
+
+    uint16_t&       operator()(uint x, uint y)       noexcept { return _up_map[ y*_n_width + x ]; }
+    uint16_t const& operator()(uint x, uint y) const noexcept { return _up_map[ y*_n_width + x ]; }
+
+private:
+    std::unique_ptr<uint16_t[]> _up_map;
+    uint _n_width;
+    uint _n_height;
+};
+
+
+_CK2_NAMESPACE_END;
+#endif
