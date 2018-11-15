0fabba28ce950e7d13930cf5e5aba05d5498bfbf
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-11-15 13:43:46 -0600

    oops

--- esc/ck2parser.py
+++ esc/ck2parser.py
@@ -62,7 +62,7 @@ def get_religions(parser, groups=True):
     religion_groups = []
     for _, tree in parser.parse_files('common/religions/*.txt'):
         for n, v in tree:
-            if n.val in 'secret_religion_visibility_trigger':
+            if n.val == 'secret_religion_visibility_trigger':
                 continue
             religion_groups.append(n.val)
             religions.extend(n2.val for n2, v2 in v
