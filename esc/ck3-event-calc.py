e9fee5ea12bb9b7dedfd36fc551cbfe2c1e16eba
Nick Escalona <nicholas.escalona@gmail.com>
2020-09-24 23:07:44 -0500

    support for reading event

--- esc/ck3-event-calc.py
+++ esc/ck3-event-calc.py
@@ -5,23 +5,45 @@ import ck3parser
 from print_time import print_time
 
 parser = ck3parser.SimpleParser()
+static_values = ck3parser.static_values(parser)
 
-ai_values = ['ai_honor', 'ai_zeal', 'ai_greed', 'ai_compassion',
-             'ai_boldness', 'ai_rationality', 'ai_vengefulness', 'ai_energy']
+ai_values = ['ai_boldness', 'ai_compassion', 'ai_greed', 'ai_energy',
+             'ai_honor', 'ai_rationality', 'ai_sociability', 'ai_vengefulness',
+             'ai_zeal']
 
 
 @print_time
 def main():
-    args = [x.casefold() for x in sys.argv[1:]]
-    traits = args
+    # py -m ck3-event-calc calm humble honest
+    # py -m ck3-event-calc -e reading 14 6 6 3 8 1 chaste zealous just
+    event, stat, traits = handle_args(sys.argv[1:])
 
     attr = get_attrs(traits)
-    output(attr)
+    stat.update(attr)
+    event_result = handlers.get(event)(event, traits, stat)
+
+    output(attr, event_result)
+
+
+def handle_args(args):
+    args = [x.casefold() for x in args]
+    event, stat, traits = None, {}, None
+    if args[0] == '-e':
+        event = args[1]
+        stat = {
+            'diplomacy': int(args[2]),
+            'martial': int(args[3]),
+            'stewardship': int(args[4]),
+            'intrigue': int(args[5]),
+            'learning': int(args[6]),
+            'piety_level': int(args[7])
+        }
+        traits = args[8:]
+    return event, stat, traits
 
 
 def get_attrs(my_traits):
     all_traits = ck3parser.traits(parser)
-    static_values = ck3parser.static_values(parser)
 
     attr = {a: 0 for a in ai_values}
 
@@ -33,10 +55,51 @@ def get_attrs(my_traits):
     return attr
 
 
-def output(attr):
+def handle_reading(event, traits, stat):
+    results = {x: 50 for x in ['religious', 'entertaining', 'informative']}
+
+    results['religious'] += stat['ai_zeal'] * 4
+    results['religious'] += stat['ai_honor'] * 2
+    results['religious'] += stat['piety_level'] * 25
+    results['religious'] += max(0, (
+        stat['learning'] - static_values['mediocre_skill_rating']) * 5)
+
+    results['entertaining'] += stat['ai_boldness'] * 2
+    results['entertaining'] += stat['ai_greed'] * 2
+    results['entertaining'] += stat['ai_sociability'] * 2
+    results['entertaining'] += max(0, (
+        stat['diplomacy'] - static_values['mediocre_skill_rating']) * 5)
+    results['entertaining'] += max(0, (
+        stat['martial'] - static_values['mediocre_skill_rating']) * 5)
+
+    results['informative'] += min(0, stat['ai_zeal'] * -2)
+    results['informative'] += max(0, (
+        stat['stewardship'] - static_values['mediocre_skill_rating']) * 5)
+    results['informative'] += (stat['learning'] -
+                               static_values['mediocre_skill_rating']) * 10
+    if 'arrogant' in traits:
+        results['informative'] -= 20
+    if 'impatient' in traits:
+        results['informative'] -= 20
+    if 'dull' in traits:
+        results['informative'] -= 20
+    if 'shrewd' in traits:
+        results['informative'] += 20
+
+    return max(results.items(), key=lambda x: x[1])[0]
+
+
+def output(attr, event_result):
+    if event_result:
+        print(event_result)
     for k, v in attr.items():
         print(f'{v:>4} {k[3:]}')
 
+
+handlers = {
+    None: lambda _: None,
+    'reading': handle_reading
+}
 
 if __name__ == '__main__':
     main()
