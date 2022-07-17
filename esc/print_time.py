9bc1f08583ca001014a8ec902d41547bc90766e1
grotaclas <grotaclas@gmail.com>
2022-07-17 20:28:34 +0200

    allow @print_time for functions with paramters

--- esc/print_time.py
+++ esc/print_time.py
@@ -7,7 +7,7 @@ def print_time(func):
     def timed_func(*args, **kwargs):
         start_time = time.time()
         try:
-            func()
+            func(*args, **kwargs)
         finally:
             end_time = time.time()
             print('Time: {:g} s'.format(end_time - start_time),
